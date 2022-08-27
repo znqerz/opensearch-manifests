402df1102bca222da52e549786908ec6ae0b706b
znqerz <73216018+znqerz@users.noreply.github.com>
2022-08-27 10:25:03 +0800

    chore: replace helm charts by kustomization config

--- scripts/make-rules/opensearch.mk
+++ scripts/make-rules/opensearch.mk
@@ -1,17 +1,5 @@
-OPENSEARCH_RELEASE_NAME="demo"
-
-helm-dependency-build:
-	cd $(OPENSEARCH_DIR) && helm dependency build 
-	cd $(OPENSEARCH_DASHBORD_DIR) && helm dependency build 
-
-generate-opensearch-cluster-manifest:
-	@cd $(OPENSEARCH_DIR) && helm template --release-name ${OPENSEARCH_RELEASE_NAME} -f values.yaml . > $(BASE_DIR)/opensearch.yaml
-
-generate-opensearch-dashboard-manifest:
-	@cd $(OPENSEARCH_DASHBORD_DIR) && helm template --release-name ${OPENSEARCH_RELEASE_NAME} -f values.yaml . > $(BASE_DIR)/opensearch-dashboard.yaml
-
-generate-opensearch-manifest-%: generate-opensearch-cluster-manifest generate-opensearch-dashboard-manifest
-	@cd $(ENVS_DIR)/$* && kustomize build
+generate-opensearch-manifest-%:
+	@cd $(ENVS_DIR)/$* && kustomize build --enable-helm
 
 apply-opensearch-%:
 	@make generate-opensearch-manifest-$* | kubectl apply -f -
